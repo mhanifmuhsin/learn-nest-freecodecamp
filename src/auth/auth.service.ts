f83d94db9071ad77ffe4302262d9ad1e077e35d5
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2023-07-30 12:24:30 +0700

    fix: :art: tambah try catch

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,27 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import * as argon from 'argon2';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
-    const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
 
-      // select: {
-      //   email: true,
-      //   createdAt: true,
-      // },
-    });
-    delete user.hash;
-    return user;
+        // select: {
+        //   email: true,
+        //   createdAt: true,
+        // },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+    }
   }
   // async signup(dto: AuthDto) {
   //   const hash = await argon.hash(dto.password);
